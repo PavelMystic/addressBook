9a9a59dc3c950a21a411b103e75fb4494dc9c1b0
PavelMystic <Kulmon@seznam.cz>
2016-12-30 20:39:29 +0100

    Add files via upload

---
+++ addressBook.cpp
@@ -0,0 +1,24 @@
+/**
+* \file main.cpp
+*
+* \ingroup none
+*
+* \brief Entry point of whole app
+*
+* No detailed description.
+*
+* \note Attempts at zen rarely work.
+*
+* \author (last to touch it) $Author: pkl $
+*
+* \version
+*
+* \date $Date: 2016/12/29 22:44:00 $
+*
+* Contact: bv@bnl.gov
+*
+* Created on: Thur Dec 12 22:44:00 2016
+*
+*/
+
+#include "wx\wx.h"
